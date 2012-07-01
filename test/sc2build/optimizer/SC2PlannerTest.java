d286d5453affefb671273d919228122bda630feb
mponomarenko <mponomarenko@gmail.com>
2012-07-01 10:41:55 +0300

    test for 2 zealot timing

--- test/sc2build/optimizer/SC2PlannerTest.java
+++ test/sc2build/optimizer/SC2PlannerTest.java
@@ -2,6 +2,8 @@ package sc2build.optimizer;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import sc2build.optimizer.SC2Planner.Entity;
@@ -21,6 +23,20 @@ public class SC2PlannerTest {
 		assertEquals(0,sc2.activeEvents);
 		assertEquals(1,sc2.getEntityByName("Slow Gas Patch").value[0]);
 		assertEquals(1,asm2.value[0]);
+	}
+	
+	@Test
+	public void test2Zealots() {
+		SC2Planner sc2 = new SC2Planner();
+		sc2.init(Faction.PROTOSS);
+		assertEquals(0,sc2.getEntityByName("Zealot").value[0]);
+		sc2.setBuild(Arrays.asList(sc2.getEntityByName("Pylon"),sc2.getEntityByName("Gateway"),sc2.getEntityByName("Zealot"),sc2.getEntityByName("Zealot")));
+		sc2.updateCenter(false, true, 0, false);
+		//sc2.dumpState(sc2);
+		assertFalse(sc2.getEntityByName("Zealot").eventualError);
+		assertEquals(0,sc2.activeEvents);
+		assertEquals(2,sc2.getEntityByName("Zealot").value[0]);
+		assertEquals((3*60+10)*100+308,sc2.currentTime);
 	}
 
 }
